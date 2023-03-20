79647f325d84aff00b300f3c7641f0606a281029
tgrech1997 <45184353+tgrech1997@users.noreply.github.com>
2023-03-20 13:14:57 +0000

    Invert light pin, add comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include <BleGamepad.h> // https://github.com/lemmingDev/ESP32-BLE-Gamepad
 
-static const int LIGHT_PIN = 21;	//ESP32 IO pins
-static const int TRIGGER_PIN = 19;
+static const int LIGHT_PIN = 21;	// ESP32 IO pins
+static const int TRIGGER_PIN = 19; // any input pins with pullups will work
 
-BleGamepad bleGamepad("NES-Zapper", "GrechTech", 100); //Initialise Bluetooth gamepad
+BleGamepad bleGamepad("NES-Zapper", "GrechTech", 100); // Initialise Bluetooth gamepad
 
 void setup()
 {
   pinMode(LIGHT_PIN, INPUT_PULLUP);
-  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+  pinMode(TRIGGER_PIN, INPUT_PULLUP); // Tomee Zapp has a simple switch NC to GND. 
+  // When trigger pulled, switch disconnected from GND allowing it to be pulled up
+
   bleGamepad.begin();
 }
 
@@ -17,7 +19,7 @@ void loop()
 {
   if (bleGamepad.isConnected())
   {
-    if(digitalRead(LIGHT_PIN))
+    if(!digitalRead(LIGHT_PIN)) //Inverted Light Pin
       bleGamepad.press(BUTTON_1);
     else
       bleGamepad.release(BUTTON_1);
